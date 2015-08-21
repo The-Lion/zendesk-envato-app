eb2b7a513d8e8e82b71e8c0e786d5a92430c79af
The-Lion <github@florisdeleeuw.nl>
2015-08-21 15:40:55 +0200

    Initial Release

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 # Zendesk Envato Integration
 
-Easily query [Envato API](http://marketplace.envato.com/api/documentation) for
-license keys.
+Easily query the [Envato API](http://marketplace.envato.com/api/documentation) for license keys.
+
+![Zendesk Envato Integration](https://raw.githubusercontent.com/The-Lion/zendesk-envato-app/master/screenshot_small.png "Zendesk Envato Integration")
+
+### Installation
+1. In Zendesk go to Admin -> Apps -> Manage -> Upload App
+2. Upload the downloaded zip file (zendesk-envato-app-master.zip) and set an App name, e.g. Envato - Verify Purchase & Support 
+3. [Create your personal token](https://build.envato.com/create-token/) for the Envato API. Select **View your items' sales history** and **Verify purchases of your items**. Insert the created token into the `Personal Token` setting of the uploaded App
+4. If you want to check the purchase code of your tickets automatically checks, enter the `Field ID` of the corresponding purchase code field. If you don't have such a ticket field yet, see below.
+
+
+### Create Purchase Code ticket field
+1. Go to Admin -> Ticket Fields and click on `add custom field`
+2. Select a `Regular Expression field`
+3. Make the field visible and editable for end-users. Add a description like: `Format: XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX. If you have a pre-sale questions, just leave it empty.`
+4. Under `Field options` set the following expression: `^([a-z0-9]{8})-?([a-z0-9]{4})-?([a-z0-9]{4})-?([a-z0-9]{4})-?([a-z0-9]{12})$`
+5. After saving the purchase field you can find the Field ID on top of its edit page. You can insert this ID in the settings of this App.
+
+
+App based on https://github.com/mattes/zendesk-envato-app
--- app.js
+++ app.js
@@ -1,22 +1,47 @@
-(function() {
+(function () {
 
   return {
     requests: {
-      verify_purchase: function(purchase_code){
+      verify_purchase: function (purchase_code) {
         return {
-          url: 'http://marketplace.envato.com/api/edge/' + this.setting('username') + '/' + this.setting('api_key') + '/verify-purchase:' + purchase_code + '.json',
-          dataType: 'json'
+          //url: 'https://api.envato.com/v2/market/author/sale?code=' + purchase_code,
+          url: 'https://api.envato.com/v1/market/private/user/verify-purchase:' + purchase_code + '.json',
+          dataType: 'json',
+          headers: {
+            'Authorization': 'Bearer ' + this.setting('token')
+          },
+          cors: true
         };
-      } 
+      }
     },
     events: {
-
-      'keypress #purchase_code': function(event) {
-        if(event.which == 13) {
+      'app.activated': function (event) {
+        if (!event.firstLoad) {
+          return;
+        }
+        var ticket = this.ticket();
+        var purchase_code = ticket.customField("custom_field_" + this.setting('purchasecode_field_id'));
+        this.$('#purchase_code').val(purchase_code);
+        if (ticket.status() === 'new' || ticket.status() === 'open') {
+          if (purchase_code && purchase_code !== '') {
+            this.ajax('verify_purchase', purchase_code);
+            this.switchTo('loading', {
+              purchase_code: purchase_code,
+              username: this.setting('username')
+            });
+          } else {
+            this.switchTo('failed', {
+              message: this.I18n.t('purchase_code.type_purchase_code')
+            });
+          }
+        }
+      },
+      'keypress #purchase_code': function (event) {
+        if (event.which == 13) {
           // enter pressed
           event.preventDefault();
           var purchase_code = this.$(event.target).val();
-          if(purchase_code && purchase_code !== '') {
+          if (purchase_code && purchase_code !== '') {
             this.ajax('verify_purchase', purchase_code);
             this.switchTo('loading', {
               purchase_code: purchase_code,
@@ -30,21 +55,37 @@
 
         }
       },
-
-      'verify_purchase.done': function(data) {
+      'verify_purchase.done': function (data) {
         // example responses:
         // {"verify-purchase":{}}
-        // {"verify-purchase":{"item_name":"NAME","item_id":"ID","created_at":"Tue Feb 26 18:51:02 +1100 2013","buyer":"USERNAME","licence":"Regular License"}}
+        // {"verify-purchase":{"item_name":"NAME","item_id":"ID","created_at":"Tue Feb 26 18:51:02 +1100 2013","buyer":"USERNAME","licence":"Regular License","supported_until":""}}
 
-        if(data) {
-          if(data.hasOwnProperty('verify-purchase')) {
-            if(data['verify-purchase'].hasOwnProperty('item_id')) {
+        if (data) {
+
+          if (data.hasOwnProperty('verify-purchase')) {
+            if (data['verify-purchase'].hasOwnProperty('item_id')) {
+
+              var support_date = data['verify-purchase'].supported_until, support_text, support_class;
+
+              if (support_date === "" || support_date === null) {
+                support_text = this.I18n.t('purchase_code.support_expired');
+                support_class = 'error';
+
+              } else {
+                support_text = this.I18n.t('purchase_code.support_valid');
+                support_date = this.I18n.t('purchase_code.support_valid_till') + ' ' + new Date('Tue Aug 18 01:09:18 +1000 2015').toLocaleString();
+                support_class = 'success';
+              }
+
               this.switchTo('verify_purchase_code_result', {
                 item_name: data['verify-purchase'].item_name,
                 item_id: data['verify-purchase'].item_id,
-                created_at: data['verify-purchase'].created_at,
+                created_at: new Date(data['verify-purchase'].created_at).toLocaleString(),
                 buyer: data['verify-purchase'].buyer,
-                licence: data['verify-purchase'].licence
+                licence: data['verify-purchase'].licence,
+                support_class: support_class,
+                support_text: support_text,
+                support_date: support_date
               });
             }
             else {
@@ -53,7 +94,7 @@
               });
             }
           }
-          else if(data.hasOwnProperty('error')) {
+          else if (data.hasOwnProperty('error')) {
             this.switchTo('failed', {
               message: data.error
             });
@@ -69,8 +110,7 @@
           });
         }
       },
-
-      'verify_purchase.fail': function() {
+      'verify_purchase.fail': function () {
         this.switchTo('failed', {
           message: this.I18n.t('purchase_code.unable_to_contact')
         });
